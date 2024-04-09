e168f0a85b03aafa90cd1d7442b4949d30617932
Sahil Ojha <59600077+sahiloj@users.noreply.github.com>
2024-04-09 23:47:13 +0545

    Update main.go

--- main.go
+++ main.go
@@ -1,56 +1,83 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"os/exec"
-	"strings"
+        "flag"
+        "fmt"
+        "io/ioutil"
+        "os"
+        "os/exec"
+        "strings"
 )
 
 func performDNSLookup(subdomain string) {
-	fmt.Printf("Performing DNS lookup for %s\n", subdomain)
-	cmd := exec.Command("dig", "@1.1.1.1", subdomain)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		fmt.Printf("Error performing DNS lookup for %s: %v\n", subdomain, err)
-		return
-	}
-	fmt.Println(string(output))
-	fmt.Println("-----------------------------------------")
+        fmt.Printf("Performing DNS lookup for %s\n", subdomain)
+        cmd := exec.Command("dig", "@1.1.1.1", subdomain)
+        output, err := cmd.CombinedOutput()
+        if err != nil {
+                fmt.Printf("Error performing DNS lookup for %s: %v\n", subdomain, err)
+                return
+        }
+        fmt.Println(string(output))
+        fmt.Println("-----------------------------------------")
 }
 
 func main() {
-	
-	subdomainsFile := flag.String("s", "", "Path to subdomains file")
-	flag.Parse()
 
-	if *subdomainsFile != "" {
-	fmt.Println(`
-     _ _                        _            
-  __| (_) __ _  /\/\   __ _ ___| |_ ___ _ __ 
+        subdomainsFile := flag.String("s", "", "Path to subdomains file")
+        showHelp := flag.Bool("h", false, "Show this help message")
+        flag.Parse()
+
+        // Check if the executable name is used as an argument
+        if len(os.Args) > 1 && (os.Args[1] == "main" || os.Args[1] == "./main") {
+                *showHelp = true
+        }
+
+        if *showHelp {
+                fmt.Println(`
+     _ _                        _
+  __| (_) __ _  /\/\   __ _ ___| |_ ___ _ __
  / _` + "`" + ` | |/ _` + "`" + ` |/    \ / _` + "`" + ` / __| __/ _ | '__|
-| (_| | | (_| / /\/\ | (_| \__ | ||  __| |   
- \__,_|_|\__, \/    \/\__,_|___/\__\___|_|   
-         |___/                               
+| (_| | | (_| / /\/\ | (_| \__ | ||  __| |
+ \__,_|_|\__, \/    \/\__,_|___/\__\___|_|
+         |___/
+
+            by @Sahil Ojha
+
+Usage:
+  main [-h] [-s subdomains_file]
+
+Options:
+  -h    Show this help message
+  -s    Path to subdomains file
+`)
+                os.Exit(0)
+        }
+
+        if *subdomainsFile != "" {
+                fmt.Println(`
+     _ _                        _
+  __| (_) __ _  /\/\   __ _ ___| |_ ___ _ __
+ / _` + "`" + ` | |/ _` + "`" + ` |/    \ / _` + "`" + ` / __| __/ _ | '__|
+| (_| | | (_| / /\/\ | (_| \__ | ||  __| |
+ \__,_|_|\__, \/    \/\__,_|___/\__\___|_|
+         |___/
 
             by @Sahil Ojha`)
-}
+        }
 
-	// Read subdomains file
-	fileData, err := ioutil.ReadFile(*subdomainsFile)
-	if err != nil {
-		fmt.Println("Error reading subdomains file:", err)
-		os.Exit(1)
-	}
+        // Read subdomains file
+        fileData, err := ioutil.ReadFile(*subdomainsFile)
+        if err != nil {
+                fmt.Println("Error reading subdomains file:", err)
+                os.Exit(1)
+        }
 
-	subdomains := strings.Split(string(fileData), "\n")
+        subdomains := strings.Split(string(fileData), "\n")
 
-	for _, subdomain := range subdomains {
-		subdomain = strings.TrimSpace(subdomain)
-		if subdomain != "" {
-			performDNSLookup(subdomain)
-		}
-	}
+        for _, subdomain := range subdomains {
+                subdomain = strings.TrimSpace(subdomain)
+                if subdomain != "" {
+                        performDNSLookup(subdomain)
+                }
+        }
 }
